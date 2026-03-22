faaaa908d38e468983095d8c8e8df8b7948df003
woro <252448881+wouterroose-tech@users.noreply.github.com>
2026-03-22 21:23:13 +0100

    cleanup

--- poc/jolc/src/test/java/tolk/runtime/TestInteropException.java
+++ poc/jolc/src/test/java/tolk/runtime/TestInteropException.java
@@ -1,10 +1,7 @@
 package tolk.runtime;
 
-import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.TruffleObject;
-import com.oracle.truffle.api.library.ExportLibrary;
 
-@ExportLibrary(InteropLibrary.class)
 public class TestInteropException extends IllegalArgumentException implements TruffleObject {
     private static final long serialVersionUID = 1L;
     public TestInteropException(String message) {

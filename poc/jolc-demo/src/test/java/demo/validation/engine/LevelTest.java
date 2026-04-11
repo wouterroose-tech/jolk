f600d5bdd1dff838060b241e38e0683c5e7e4e17
woro <252448881+wouterroose-tech@users.noreply.github.com>
2026-04-11 11:04:40 +0200

    cleanup

---
+++ poc/jolc-demo/src/test/java/demo/validation/engine/LevelTest.java
@@ -0,0 +1,18 @@
+package demo.validation.engine;
+
+import org.junit.jupiter.api.Test;
+
+public class LevelTest {
+
+    
+    @Test
+    public void testLevel() {
+        // testing the Jolk class through the polyglot interop
+        /*
+        Level level = Level.ERROR;
+        assertTrue(level.isError());
+        assertFalse(level.isWarning());
+        assertEquals("ERROR", level.name());
+        */
+    }
+}

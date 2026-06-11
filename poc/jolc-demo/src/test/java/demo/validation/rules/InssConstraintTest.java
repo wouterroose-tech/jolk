37fe8b338a4c46e1b284dd8373495c39f9128267
woro <252448881+wouterroose-tech@users.noreply.github.com>
2026-06-11 21:52:27 +0200

    test demo

---
+++ poc/jolc-demo/src/test/java/demo/validation/rules/InssConstraintTest.java
@@ -0,0 +1,67 @@
+package demo.validation.rules;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.graalvm.polyglot.Value;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class InssConstraintTest  extends ValidationTestBase {
+
+
+    Value testInstance;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        // Create the Jolk test instance
+        Value testclass = getJolkClass("/test/validation/rules/InssConstraintTest.jolk");
+        testInstance = testclass.invokeMember("new");
+    }
+    
+    @Test
+    public void test_accept_success() {
+        assertTrue(testInstance.invokeMember("test_accept_success").asBoolean());
+    }
+
+    @Test
+    public void test_accept_failure() {
+        assertTrue(testInstance.invokeMember("test_accept_failure").asBoolean());
+    }
+
+    @Test
+    public void test_satisfiesPreCondition_success() {
+        assertTrue(testInstance.invokeMember("test_satisfiesPreCondition_success").asBoolean());
+    }
+
+    @Test
+    public void test_satisfiesPreCondition_failure() {
+        assertTrue(testInstance.invokeMember("test_satisfiesPreCondition_failure").asBoolean());
+    }
+
+    @Test
+    public void test_isValid_success() {
+        assertTrue(testInstance.invokeMember("test_isValid_success").asBoolean());
+    }
+
+    @Test
+    public void test_isValid_failure() {
+        assertTrue(testInstance.invokeMember("test_isValid_failure").asBoolean());
+    }
+
+    @Test
+    public void test_isValid_logic() {
+        assertTrue(testInstance.invokeMember("test_isValid_logic").asBoolean());
+    }
+
+    @Test
+    public void test_getIssue() {
+        assertTrue(testInstance.invokeMember("test_getIssue").asBoolean());
+    }
+
+    @Test
+    public void test_interrupt() {
+        assertTrue(testInstance.invokeMember("test_interrupt").asBoolean());
+    }
+
+}
---
+++ poc/jolc/src/test/java/demo/validation/rules/InssConstraintTest.java
@@ -0,0 +1,177 @@
+package demo.validation.rules;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.graalvm.polyglot.Value;
+import org.junit.jupiter.api.Test;
+
+import tolk.JolcTestBase;
+
+public class InssConstraintTest  extends JolcTestBase {
+
+    private Value issueType() {
+        String source = """
+            ~ demo.validation.engine;
+            record Issue {
+                Object subject;
+                String message;
+                Level level;
+                Boolean concerns(Object subject) { ^ self #subject == subject }
+                Boolean match(Level level) { ^ self #level == level }
+            }""";
+        return eval(source);
+    }
+
+    private Value levelEnum() {
+        String source = """
+            ~ demo.validation.engine;
+            enum Level {
+                ERROR; WARNING; INFO; DEBUG;
+                Boolean isError() { ^ self == ERROR }
+            }""";  
+        return eval(source);
+    }
+
+    private Value context() {
+        String source = """
+            & java.util.function.Predicate;
+            & java.util.ArrayList;
+            & demo.validation.engine.Level.WARNING;
+            class ExecutionContext {
+                stable ArrayList<Issue> issues = #[];
+                Self add(Object subject, Issue issue) {
+                    self #issues #add(issue)
+                }
+                Boolean hasIssue() {
+                    ^ !self #issues #isEmpty
+                }
+                Boolean hasError() {
+                    ^ self #hasMatch [i -> i #level #isError ]
+                }
+                Boolean hasWarning() {
+                    ^ self #hasMatch [i -> i #match(WARNING) ]
+                }
+                Boolean hasIssue(Object subject) {
+                    ^ self #hasMatch [i -> i #concerns(subject)]
+                }
+                private Boolean hasMatch(Predicate<Issue> p) {
+                    ^ self #issues #anyMatch(p)
+                }
+            }""";  
+        return eval(source);
+    }
+
+    private Value interrupt() {
+        String source = """
+            ~ test;
+            class Interrupt extends RuntimeException {
+                meta constant Interrupt HALT = Interrupt #new;
+                meta Interrupt new() {
+                    ^super #new("Validation Halt", null, false, false)
+                }   
+            }""";
+        return eval(source);
+    }
+
+    private Value validation() {
+        String source = """
+            package abstract class Validation<T> {
+                protected Boolean satisfiesPreCondition(T subject, ExecutionContext executionContext) {
+                    ^ true
+                }
+                @Override
+                package final accept(T subject, ExecutionContext executionContext) {
+                    self #satisfiesPreCondition(subject, executionContext) ? [ self #doAccept(subject, executionContext) ]
+                }
+                package abstract doAccept(T subject, ExecutionContext executionContext);
+                protected Interrupt interrupt() {
+                    ^ Interrupt #NO_INTERRUPT
+                }
+            }""";
+        return eval(source);
+    }
+
+    private Value constraint() {
+        String source = """
+            abstract class Constraint<T> extends Validation<T> {
+                package final doAccept(T subject, ExecutionContext executionContext) {
+                    System #out #println("### reached doAccept");
+                    #isValid(subject) ? [ ^ self ];
+                    executionContext #add(subject, #getIssue(subject, executionContext));
+                    self #interrupt #ifPresent [ i -> i #throw ]
+                }
+                protected abstract Boolean isValid(T subject);
+                protected abstract Issue getIssue(T subject, ExecutionContext executionContext);
+            }""";
+        return eval(source);
+    }
+
+    private Value inssConstraint() {
+        String source = """
+            #! class InssConstraint extends Constraint<Person> {
+                #: Boolean isValid(Person person) {
+                    ^ self #isValid(person #ssn)
+                }
+                #> Boolean isValid(Long ssn) {
+                    ^ 97 - ((ssn / 100) % 97) == (ssn % 100)
+                }
+                #: Issue getIssue(Person person,  ExecutionContext executionContext) {
+                    ^ Issue #new(person, "INSS_INVALID",  Level #ERROR)
+                }
+                #: Interrupt interrupt() {
+                    ^ FORM_INTERRUPT
+                }
+            }""";
+        return eval(source);
+    }
+
+    private Value person() {
+        String source = """
+            public class Person {
+                Long ssn;
+            }""";
+        return eval(source);
+    }
+
+    private Value testInssConstraint() {
+        String source = """
+            public class Test {
+                Boolean test() {
+                    ExecutionContext context = ExecutionContext #new;
+                    // Use a valid SSN logic: 800101035 -> 97 - (8001010 % 97) = 35
+                    Person person = Person #new #ssn(800101035);
+                    InssConstraint #new #accept(person, context);
+                    ^ !context #hasIssue
+                }
+            }""";
+        return eval(source);
+    }
+
+    @Test
+    void testParsing() {
+        this.issueType();
+        this.context();
+        this.levelEnum();
+        this.person();
+        this.interrupt();
+        this.constraint();
+        this.inssConstraint();
+        this.testInssConstraint();
+    }
+
+    @Test
+    void test() {
+        this.issueType();
+        this.context();
+        this.levelEnum();
+        this.person();
+        this.interrupt();
+        this.validation();
+        this.constraint();
+        this.inssConstraint();
+        Value test = this.testInssConstraint().invokeMember("new");
+        assertTrue(test.invokeMember("test").asBoolean());
+    }
+
+}
